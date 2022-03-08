5edd181dc29004d7df2bf5424c45875015ade682
Huerta <huertaaraujo@gmail.com>
2022-03-08 11:03:25 -0300

    alguns comandos importantes

--- comandos/comandos.py
+++ comandos/comandos.py
@@ -1,7 +1,12 @@
 # Criar ambiente virtual --> python3 -m venv venv
 # Ativar ambiente virtual --> .\venv\Scripts\activate
 # Desativar ambiente virtual --> deactivate
-# Para usar o ambiente virtual já criado em alguma outra pasta - ctrl + shift + p: select interpreter python (selecionar o com a estrelinha)
+# Para usar o ambiente virtual já criado em alguma outra pasta:
+# "ctrl + shift + p" - Select interpreter python (selecionar o favoritado)
 
 # Atualizar pip: python3 -m pip install --upgrade pip
 # Outra atualização: python -m pip install pip setuptools wheel --upgrade
+
+# django-admin --help
+
+# noqa

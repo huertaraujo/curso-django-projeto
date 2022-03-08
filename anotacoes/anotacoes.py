5d50ef60e078ba6ea4797a8c3c5866275056801b
Huerta <huertaaraujo@gmail.com>
2022-03-08 12:34:16 -0300

    teoria

---
+++ anotacoes/anotacoes.py
@@ -0,0 +1,4 @@
+""" A criação de um ambiente vitual é necessário, pois garante um ambiente
+    exclusivo para o seu projeto. Possibilitando utilizar diferentes versões
+    do Django para cada projeto.
+"""

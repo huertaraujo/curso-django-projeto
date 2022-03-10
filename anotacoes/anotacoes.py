edfbec956e80e4f073752c2519593b217ba9580f
Huerta <huertaaraujo@gmail.com>
2022-03-10 10:12:39 -0300

    anotações do processo

--- anotacoes/anotacoes.py
+++ anotacoes/anotacoes.py
@@ -2,3 +2,8 @@
     exclusivo para o seu projeto. Possibilitando utilizar diferentes versões
     do Django para cada projeto.
 """
+
+"""
+Depois da criação do projeto pelo terminal, em urls criar funções.
+
+"""

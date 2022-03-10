a346f74ba5bda1617c977644af73c42e0a13ef1a
Huerta <huertaaraujo@gmail.com>
2022-03-10 10:09:13 -0300

    alguns comandos importantes

--- comandos/comandos.py
+++ comandos/comandos.py
@@ -1,18 +1,33 @@
-# Criar ambiente virtual --> python3 -m venv venv
-# Ativar ambiente virtual --> .\venv\Scripts\activate
-# Desativar ambiente virtual --> deactivate
-# Para usar o ambiente virtual já criado em alguma outra pasta:
+# CRIAR AMBIENTE VIRTUAL:
+# python3 -m venv venv
+
+# ATIVAR AMBIENTE VIRTUAL:
+# .\venv\Scripts\activate
+
+# DESATIVAR AMBIENTE VIRTUAL:
+# deactivate
+
+# Para usar o ambiente virtual já criado em alguma outra pasta
+# (INICIAÇÃO AUTOMÁTICA NO TERMINAL):
 # "ctrl + shift + p" - Select interpreter python (selecionar o favoritado)
 
-# Atualizar pip: python3 -m pip install --upgrade pip
+# ATUALIZAR PIP: python3 -m pip install --upgrade pip
 # Outra atualização: python -m pip install pip setuptools wheel --upgrade
+
 
 # django-admin --help
 # python -m django
 
-# Iniciar um projeto django --> django-admin startproject projeto .
-# Subir servidor django em modo de desenvolvimento:
+# INICIAR UM PROJETO DJANGO:
+# django-admin startproject projeto .
+
+# SUBIR SERVIDOR DJANGO em modo de desenvolvimento:
 # python manage.py runserver (em vez de usar o django-admin, usar sempre o
 # manage.py)
+
+# Criar funções na pasta urls (vai ser apresentado na página)
+
+# CRIAR UM APP (DIVISÃO EM ABAS, SETORES):
+# python manage.py startapp technology
 
 # noqa
